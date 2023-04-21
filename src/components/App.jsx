5693dea0055084b408227b90592dd005965e9ca3
Stepan Skichko <st.skichko@gmail.com>
2023-04-21 18:40:19 +0300

    http request works

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,30 @@
 import { Component } from 'react';
+import axios from 'axios';
 import { Searchbar } from './Searchbar/Searchbar';
 
 export class App extends Component {
   state = {
     keyword: '',
+    imgArray: [],
   };
 
-  searchByKeyword = keyword => {
-    console.log(keyword);
+  searchByKeyword = key => {
+    const keywordValue = Object.values(key);
+    this.setState({ keyword: keywordValue });
   };
+
+  async componentDidUpdate() {
+    if (this.state.keyword !== '') {
+      try {
+        const response = await axios.get(
+          `https://pixabay.com/api/?q=${this.state.keyword}&page=1&key=34039766-687567eb1e3c3ba001a14a80f&image_type=photo&orientation=horizontal&per_page=12`
+        );
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
 
   render() {
     return <Searchbar onSubmit={this.searchByKeyword} />;
---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,5 @@
+// export const ContactList = ({ contacts, onDelete }) => {
+//   return (
+
+//   );
+// };
