50165aee61deb352e1e7a43e8e68e275e43b10f6
Stepan Skichko <st.skichko@gmail.com>
2023-04-22 13:59:46 +0300

    loader added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,17 +3,22 @@ import axios from 'axios';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+
+const BACE_URL = 'https://pixabay.com/api';
+const API_KEY = '34039766-687567eb1e3c3ba001a14a80f';
 
 export class App extends Component {
   state = {
     keyword: '',
     images: [],
     page: 1,
+    loading: false,
   };
 
   searchByKeyword = key => {
     const keywordValue = Object.values(key);
-    this.setState({ keyword: keywordValue, page: 1 });
+    this.setState({ keyword: keywordValue, page: 1, loading: false });
   };
 
   onLoadMoreClick = () => {
@@ -27,12 +32,14 @@ export class App extends Component {
       prevState.keyword !== this.state.keyword ||
       prevState.page !== this.state.page
     ) {
+      this.setState({ loading: true });
       try {
         const response = await axios.get(
-          `https://pixabay.com/api/?q=${this.state.keyword}&page=${this.state.page}&key=34039766-687567eb1e3c3ba001a14a80f&image_type=photo&orientation=horizontal&per_page=12`
+          `${BACE_URL}/?q=${this.state.keyword}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
         );
         this.setState(prevState => ({
           images: [...prevState.images, ...response.data.hits],
+          loading: false,
         }));
       } catch (error) {
         console.error(error);
@@ -41,12 +48,12 @@ export class App extends Component {
   }
 
   render() {
-    const { images, keyword } = this.state;
+    const { images, keyword, loading } = this.state;
     return (
       <div>
         <Searchbar onSubmit={this.searchByKeyword} />
         <ImageGallery images={images} />
-
+        {loading && <Loader />}
         {keyword !== '' && <Button onClick={this.onLoadMoreClick} />}
       </div>
     );
